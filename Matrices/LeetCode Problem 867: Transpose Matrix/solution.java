6cdd3b8861d4dbcf858ff14f8db846167e76d0fc
Alok Kumar <alokkumarswn01@gmail.com>
2025-01-26 20:34:14 +0530

    Create solution.java

---
+++ Matrices/LeetCode Problem 867: Transpose Matrix/solution.java
@@ -0,0 +1,15 @@
+// LeetCode Problem 867: Transpose Matrix
+// Link: https://leetcode.com/problems/transpose-matrix/
+
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int R = matrix.length, C = matrix[0].length;
+        int[][] ans = new int[C][R];
+        for (int r = 0; r < R; ++r) {
+            for (int c = 0; c < C; ++c) {
+                ans[c][r] = matrix[r][c];
+            }
+        }
+        return ans;
+    }
+}
